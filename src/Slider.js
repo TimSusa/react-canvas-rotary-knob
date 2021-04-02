72661e47958492a1d6f84f448c884392130e0790
TimSusa <timmsusa@googlemail.com>
2021-04-02 22:36:24 +0200

    feat: inc / dec on every pointer pos

--- src/Slider.js
+++ src/Slider.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 
 function Slider({ width = 160, height = 160, value = 0.5 }) {
-  const $window = window || {};
+  //const $window = window || {};
   const canvasRef = useRef(null);
   let context = useRef(null);
   let animationFrameId = useRef(null);
   const [val, setVal] = useState(value);
   const isDragging = useRef(false);
   const verticalDiff = useRef(0);
+  const lastOffset = useRef(0);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -15,7 +16,7 @@ function Slider({ width = 160, height = 160, value = 0.5 }) {
     context.current = canvas.getContext("2d");
     var radius = canvas.height / 2;
     context.current.translate(radius, radius);
-    draw(0);
+    draw(valToY(val));
   }, []);
 
   return (
@@ -33,35 +34,48 @@ function Slider({ width = 160, height = 160, value = 0.5 }) {
     </div>
   );
 
-  function yToVal(y: number) {
-    const val = y / canvasRef.current.height ;
-    const tmpVal = val < 0 ? 0 : val > 1 ? 1 : val;
-    const ttval = isNaN(tmpVal) ? value : tmpVal;
-
+  function yToVal(y) {
+    const val = y / canvasRef.current.height;
+    //const tmpVal = val < 0 ? 0 : val > 1 ? 1 : val;
+    const ttval = isNaN(val) ? value : val;
+    console.log('yToVal ', val, y, ttval)
     return ttval;
   }
 
-  function draw(vDiff: number) {
+
+  function valToY(val) {
+    const tmpVal = canvasRef.current.height * val
+    //console.log('valToY ', val, tmpVal)
+    return tmpVal
+  }
+
+  function draw(vDiff) {
     const ctx = context.current;
     if (!ctx.canvas) return;
-    const val = -yToVal(vDiff);
-    const v = val + 1;
-    setVal(v);
-    if (isNaN(val)) return;
-    drawHand(ctx, val * Math.PI * 2);
+    const val = yToVal(vDiff);
+    setVal(val);
+    drawHand(ctx, -val * Math.PI*2 );
   }
 
   function handleDown(ev) {
-    console.log('handle down ', ev.nativeEvent.offsetY )
-    isDragging.current = true;
+    console.log('handle down ', ev.nativeEvent.offsetY)
     //animationFrameId.current = $window.requestAnimationFrame(draw);
+
     canvasRef.current.setPointerCapture(ev.pointerId);
-    verticalDiff.current = ev.nativeEvent.offsetY ;
+    // if (isDragging.current === false) {
+    //   verticalDiff.current = ev.nativeEvent.offsetY ;
+    // } 
+
+
+    verticalDiff.current = ev.nativeEvent.offsetY;
+    isDragging.current = true;
   }
 
   function handleMove(e) {
     if (isDragging.current === true) {
-      const tV = e.nativeEvent.offsetY - verticalDiff.current 
+      const tV = -e.nativeEvent.offsetY + verticalDiff.current + lastOffset.current
+      console.log('last offset ', tV, lastOffset.current)
+
       draw(tV);
     }
   }
@@ -69,26 +83,27 @@ function Slider({ width = 160, height = 160, value = 0.5 }) {
   function handleCancel(ev) {
     canvasRef.current.releasePointerCapture(ev.pointerId);
     //$window.cancelAnimationFrame(animationFrameId.current);
+    lastOffset.current = valToY(val)
     isDragging.current = false;
   }
 
-  function drawHand(ctx, pos: number) {
+  function drawHand(ctx, pos) {
     ctx.clearRect(
       -canvasRef.current.width / 2,
       -canvasRef.current.height / 2,
       width,
       height,
     );
-
+    console.log('rot ', pos)
     ctx.beginPath();
     ctx.lineWidth = 4;
     ctx.lineCap = "round";
     ctx.arc(0, 0, canvasRef.current.width / 2, 0, Math.PI * 2, true);
     ctx.moveTo(0, 0);
-    ctx.rotate(pos);
+    ctx.rotate(-pos);
     ctx.lineTo(0, -height / 2);
     ctx.stroke();
-    ctx.rotate(-pos);
+    ctx.rotate(pos);
   }
 }
 
